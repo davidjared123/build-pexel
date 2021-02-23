da52496ae73f3456317e577bf596b8bbb45d8178
davidjared123 <davidjared123@gmail.com>
2021-02-23 17:43:57 -0400

    build pexel on react js

---
+++ precache-manifest.0491d1e310f200a7c500290f0756f3b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d6b04eaa8adb6397ca77870a6e733f",
+    "url": "/pixel/index.html"
+  },
+  {
+    "revision": "eb881153e845463fd82f",
+    "url": "/pixel/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "47d1123910aed5519261",
+    "url": "/pixel/static/js/2.88a3299e.chunk.js"
+  },
+  {
+    "revision": "eb881153e845463fd82f",
+    "url": "/pixel/static/js/main.3e58f1df.chunk.js"
+  },
+  {
+    "revision": "3204fff308179d05496b",
+    "url": "/pixel/static/js/runtime-main.42af9878.js"
+  }
+]);
